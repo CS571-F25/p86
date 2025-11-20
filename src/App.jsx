85a473fa979da78b7db96930ae158ec700ba0f27
Bani <bgulati2@wisc.edu>
2025-11-19 22:53:12 -0600

    10:53

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { HashRouter, Route, Routes } from 'react-router'
 import './App.css'
 import Home from './components/Home'
 import AboutMe from './components/AboutMe'
+import WeeklySpread from './components/WeeklySpread'
+import AppNavbar from './components/AppNavbar'
 
 function App() {
   return <HashRouter>
+    <AppNavbar />
     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutMe />} />
-      </Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<AboutMe />} />
+      <Route path="/weekly" element={<WeeklySpread />} />
+    </Routes>
   </HashRouter>
 }
 
---
+++ src/components/HighlightCard.jsx
@@ -0,0 +1,6 @@
+export default function WeeklySpread() {
+  return <div>
+      <h1>Weekly Spread</h1>
+      <p>This is where weekly scrapbook entries will go!</p>
+    </div>
+}
